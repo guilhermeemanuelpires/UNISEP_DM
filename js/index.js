96b352a6a50c7a750a705bfdd34af852e73eec9f
Guilherme Pires <guilhermemeanuel45@gmail.com>
2026-08-18 19:48:58 -0300

    conceitos básicos de Javascript

---
+++ js/index.js
@@ -0,0 +1,28 @@
+
+console.log(typeof teste);
+
+var a = "AAAAAAA";
+var b = 100;
+var teste = "teste";
+
+// console.log(Number(a) + b);
+
+// number
+//  -- 1 2 3 4.5 3.2 -1 0
+// string
+//  --"texto, qualquer texto" ''
+// boolean
+//  true false
+// array
+//  [1,2,3,4,5]
+// object
+// {
+//     nome: "Guilherme",
+//     cpf: "99999999999",
+//     telefone: "9999999999"
+// }
+
+// type undefinid
+// representa nada
+
+
